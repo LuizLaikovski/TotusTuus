9ef69d23d4a47c8604a93abbd20c088f5e71039a
Luiz Laikovski <luizlaikovski@gmail.com>
2025-10-20 08:27:30 -0300

    Arrumando o AOS

--- react/TotusTuus/src/App.tsx
+++ react/TotusTuus/src/App.tsx
@@ -7,12 +7,17 @@ import Header from './components/header/header';
 import Hero from './components/Hero/Hero';
 import Join from './components/join/join';
 import 'aos/dist/aos.css';
+import Aos from 'aos';
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
 
   // Simula o carregamento da página
   useEffect(() => {
+    Aos.init({
+      duration: 600,
+    });
+
     const timer = setTimeout(() => {
       setIsLoading(false);
     }, 1000); // 1 segundo de delay para o preloader
--- react/TotusTuus/src/components/Hero/Hero.tsx
+++ react/TotusTuus/src/components/Hero/Hero.tsx
@@ -1,13 +1,9 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Hero.css';
-import { useEffect, useState, useRef } from 'react';
+import { useRef } from 'react';
 import { faCalendarDays, faCircleExclamation, faCircleXmark, faHandsPraying, faLocationDot, faMugHot, faShirt } from '@fortawesome/free-solid-svg-icons';
 
 function Hero() {
-    const [days, setDays] = useState('00');
-    const [hours, setHours] = useState('00');
-    const [minutes, setMinutes] = useState('00');
-    const [seconds, setSeconds] = useState('00');
     const revealSectionRef = useRef<HTMLDivElement>(null);
     const confettiContainerRef = useRef<HTMLDivElement>(null);
 
@@ -55,57 +51,14 @@ function Hero() {
         overlay.addEventListener('click', closeModal);
     };
 
-    useEffect(() => {
-        const targetDate = new Date('2025-10-18T08:10:00').getTime();
-
-        const updateCountdown = () => {
-            const now = new Date().getTime();
-            const distance = targetDate - now;
-
-            const daysRemaining = Math.floor(distance / (1000 * 60 * 60 * 24));
-            const hoursRemaining = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            const minutesRemaining = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            const secondsRemaining = Math.floor((distance % (1000 * 60)) / 1000);
-
-            setDays(daysRemaining.toString().padStart(2, '0'));
-            setHours(hoursRemaining.toString().padStart(2, '0'));
-            setMinutes(minutesRemaining.toString().padStart(2, '0'));
-            setSeconds(secondsRemaining.toString().padStart(2, '0'));
-        };
-
-        updateCountdown();
-        const interval = setInterval(updateCountdown, 1000);
-
-        return () => clearInterval(interval);
-    }, []);
-
     return (
         <>
             <section id="home" className="hero">
                 <div className="particles" id="particles"></div>
                 <div className="container">
-                    <div className="hero-content" data-aos="fade-up">
-                        <h1>Totus Tuus Marie</h1>
-                        <p>Acompanhe a contagem para a nossa grande aventura no Beto Carrero World!</p>
-
-                        {/* <div className="countdown">
-                            <div className="countdown-item">
-                                <div className="countdown-number">{days}</div>
-                                <div className="countdown-label">Dias</div>
-                            </div>
-                            <div className="countdown-item">
-                                <div className="countdown-number">{hours}</div>
-                                <div className="countdown-label">Horas</div>
-                            </div>
-                            <div className="countdown-item">
-                                <div className="countdown-number">{minutes}</div>
-                                <div className="countdown-label">Minutos</div>
-                            </div>
-                            <div className="countdown-item">
-                                <div className="countdown-number">{seconds}</div>
-                                <div className="countdown-label">Segundos</div>
-                            </div>
-                        </div> */}
+                    <div className="hero-content">
+                        <h1 data-aos="fade-up">Totus Tuus Marie</h1>
+                        <p data-aos="fade-up">Acompanhe a contagem para a nossa grande aventura no Beto Carrero World!</p>
 
                         <div className="countdown-buttons">
                             <a href="#about" className="btn btn-primary">Saiba Mais</a>
